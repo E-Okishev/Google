985ddb4581b7f26731f0cc35b7b6caa4ff2baa96
Egor Okishev <e-okishev@mail.ru>
2023-10-17 21:13:39 +0300

    add clear button func

--- src/main.js
+++ src/main.js
@@ -1,2 +1,30 @@
 import "../src/style.scss"
-import "./modules/header"
+import "./modules/headerHTML"
+
+const input = document.querySelector('.input');
+const searchBtn = document.querySelector('.search-btn');
+const clearBtn = document.querySelector('.clear-btn');
+
+clearBtn.addEventListener('click', () => {
+  input.value = ''
+  hiddenElem(clearBtn)
+})
+
+searchBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  window.open('https://google.com/', '_blank')
+})
+
+input.addEventListener('input', (evt) => {
+  let showClearBtn = evt.target.value ? showElem(clearBtn) : hiddenElem(clearBtn);
+  return showClearBtn;
+})
+
+function hiddenElem(elem) {
+  elem.classList.add('display-none');
+}
+
+function showElem(elem) {
+  elem.classList.remove('display-none')
+}
