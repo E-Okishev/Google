baadf525f7daa99c0a442cb160204a1cd4bca44d
Egor Okishev <e-okishev@mail.ru>
2023-10-19 19:56:03 +0300

    script optimization

--- src/main.js
+++ src/main.js
@@ -3,16 +3,19 @@ import "../src/style.scss"
 const input = document.querySelector('.input');
 const submitBtn = document.querySelector('.submit-btn');
 const clearBtn = document.querySelector('.clear-btn');
+const doodle = document.querySelector('.doodle-btn');
 
-clearBtn.addEventListener('click', () => {
-  input.value = ''
-  hiddenElem(clearBtn)
-})
-
-submitBtn.addEventListener('click', (evt) => {
+window.addEventListener('click', (evt) => {
   evt.preventDefault();
-  if (input.value) {
+  if (evt.target === doodle) {
+    window.open(`https://www.google.com/doodles`, '_blank')
+  }
+  if (evt.target === submitBtn && input.value) {
     window.open(`https://google.com/search?q=${input.value}`, '_blank')
+  }
+  if (evt.target.closest('.clear-btn')) {
+    input.value = ''
+    hiddenElem(clearBtn)
   }
 })
 
